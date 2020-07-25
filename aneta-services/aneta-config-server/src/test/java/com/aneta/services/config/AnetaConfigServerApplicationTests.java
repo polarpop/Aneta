1ce3a078dc55e4cdb916acf95c49dbacc3e86d62
Douglas Schamberg <doug.schamberg@gmail.com>
2020-07-25 08:58:24 -0700

    Added
    1. Zuul Server
    2. Config Server
    3. Eureka Server

---
+++ aneta-services/aneta-config-server/src/test/java/com/aneta/services/config/AnetaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aneta.services.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnetaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
