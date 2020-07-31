374914689dfb915d4ae13e90782b2ccd235c3fa8
Douglas Schamberg <doug.schamberg@gmail.com>
2020-07-31 04:54:47 -0700

    Updated Services

---
+++ services/extensions-service/src/test/java/com/aneta/services/extensions/ExtensionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aneta.services.extensions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtensionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
